7ee5d35119cdb7ea9ea0715c471085aaa9e84c33
KetyaNop <ketya.n@ligercambodia.org>
2020-03-12 14:32:50 +0700

    Added Camera Permission Prompt

--- app/src/main/java/com/example/android/surtle/MainActivity.java
+++ app/src/main/java/com/example/android/surtle/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.surtle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyPermission();
+    }
+
+    //verify camera permission
+    private void verifyPermission() {
+        Log.d("TAG", "verifyPermission: asking user for permissions");
+        String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
     }
 }
