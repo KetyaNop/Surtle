0e70678e30ef614e2944f2a451b0172417822601
KetyaNop <ketya.n@ligercambodia.org>
2020-03-18 20:45:41 +0700

    Create Login Activity

---
+++ app/src/main/java/com/example/android/surtle/login.java
@@ -0,0 +1,14 @@
+package com.example.android.surtle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
