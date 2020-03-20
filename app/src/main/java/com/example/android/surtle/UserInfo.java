e58364239ba1b05d36345d18be018dc37e474db0
KetyaNop <ketya.n@ligercambodia.org>
2020-03-20 12:43:46 +0700

    Create UserInfo.java
    Userinfo Variable

---
+++ app/src/main/java/com/example/android/surtle/UserInfo.java
@@ -0,0 +1,43 @@
+package com.example.android.surtle;
+
+import android.app.Application;
+import android.net.Uri;
+
+public class UserInfo extends Application {
+    private String Uid;
+    private String name;
+    private String email;
+    private Uri photoUrl;
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Uri getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(Uri photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
